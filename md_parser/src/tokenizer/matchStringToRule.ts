587e505662df4384728cdefda6f34493adbbdc5d
nosharp <contact@nosharp.cc>
2024-02-09 22:07:09 +0000

    refactor: match string to rule

--- md_parser/src/tokenizer/matchStringToRule.ts
+++ md_parser/src/tokenizer/matchStringToRule.ts
@@ -1,6 +1,12 @@
 import { None, Option, Some } from "../optional";
 import { TokenizerRule, TokenizerRuleHit } from "./tokenizerRule";
 
+/**
+ * uses the order of rules.
+ * @param input 
+ * @param rules 
+ * @returns 
+ */
 export default function matchStringToRule(input: string, rules: TokenizerRule[]): Option<TokenizerRuleHit>{
   // enforce order of array
   for(let i = 0; i < rules.length; i++){
