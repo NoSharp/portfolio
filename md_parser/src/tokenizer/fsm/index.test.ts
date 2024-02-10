c0e7aa016dbc42fd9496d2d6ee723a7cc4ab884e
nosharp <contact@nosharp.cc>
2024-02-10 12:14:10 +0000

    feat: add test case for varargs on fsm

--- md_parser/src/tokenizer/fsm/index.test.ts
+++ md_parser/src/tokenizer/fsm/index.test.ts
@@ -18,6 +18,20 @@ describe("Finite State Machine", () => {
     expect(result.ok && result.value[0] === 20);
   });
 
+  test("it correctly passes multiple arguments through the machine", () => {
+    const FSMTest = new FSM<[number, number], STATES, "START", "STOP"> ("START", "STOP")
+      .addState("START", (amount: number, arg2: number) => {
+        return ["HALT", amount + 10, arg2 + 50];
+      })
+      .addState("HALT", (amount: number, arg2: number) => {
+        return ["STOP", amount, arg2];
+      });
+    
+    const result = FSMTest.start(10, 10);
+
+    expect(result.ok && result.value[0] === 20 && result.value[1] === 60);
+  });
+
   test("it prevents going to the start state", () => {
     const FSMTest = new FSM<[number], STATES, "START", "STOP"> ("START", "STOP")
       .addState("START", (amount: number) => {
